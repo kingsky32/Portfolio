134836a6a7b8075986421018379cbdb91ff5dab0
seungju <kingsky32@gmail.com>
2020-09-29 19:09:06 +0900

    Update Menu Button Animation

--- src/Components/MenuButton.js
+++ src/Components/MenuButton.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { TweenLite, Power1 } from "gsap";
 
 const Line = styled.span`
   display: block;
@@ -8,7 +9,6 @@ const Line = styled.span`
   background-color: ${({ theme }) => theme.greyColor};
   text-indent: -999999px;
   position: absolute;
-  ${({ theme }) => theme.transition};
   &:nth-child(1) {
     top: 0;
   }
@@ -28,28 +28,94 @@ const Container = styled.div`
   top: 5rem;
   right: 5rem;
   cursor: pointer;
-  &:hover {
-    height: 2.5rem;
-    ${Line} {
-      &:nth-child(1) {
-        top: -.2rem;
-      }
-      &:nth-child(2) {
-        top: .9rem;
-      }
-      &:nth-child(3) {
-        top: 2rem;
-      }
-    }
-  }
 `;
 
 const MenuButton = ({ isOpenMenu, onToggleMenu }) => {
+  const [isOpenMenuLoading, setIsOpenMenuLoading] = useState(false);
+  const onClick = () => {
+    if (!isOpenMenuLoading) {
+      setIsOpenMenuLoading(true);
+      setTimeout(() => {
+        setIsOpenMenuLoading(false);
+      }, 500);
+      onToggleMenu();
+      if (isOpenMenu) {
+        TweenLite.to("#ln1, #ln3", {
+          duration: 0.3,
+          rotate: 0,
+          ease: Power1
+        });
+        TweenLite.to("#ln2", {
+          duration: 0.3,
+          rotate: 0,
+          ease: Power1
+        });
+        TweenLite.to("#ln1", {
+          delay: 0.5,
+          duration: 0.3,
+          top: "0",
+          ease: Power1
+        });
+        TweenLite.to("#ln3", {
+          delay: 0.5,
+          duration: 0.3,
+          top: "1.8rem",
+          ease: Power1
+        });
+      } else {
+        TweenLite.to("#ln1, #ln3", {
+          duration: 0.3,
+          top: ".9rem",
+          ease: Power1
+        });
+        TweenLite.to("#ln1, #ln3", {
+          delay: 0.5,
+          duration: 0.3,
+          rotate: 45,
+          ease: Power1
+        });
+        TweenLite.to("#ln2", {
+          delay: 0.5,
+          duration: 0.3,
+          rotate: -45,
+          ease: Power1
+        });
+      }
+    }
+  };
+  const onMouseEnter = () => {
+    if (!isOpenMenuLoading && !isOpenMenu) {
+      TweenLite.to("#ln1", {
+        duration: 0.3,
+        top: "-.2rem",
+        ease: Power1
+      });
+      TweenLite.to("#ln3", {
+        duration: 0.3,
+        top: "2rem",
+        ease: Power1
+      });
+    }
+  };
+  const onMouseLeave = () => {
+    if (!isOpenMenuLoading && !isOpenMenu) {
+      TweenLite.to("#ln1", {
+        duration: 0.3,
+        top: "0",
+        ease: Power1
+      });
+      TweenLite.to("#ln3", {
+        duration: 0.3,
+        top: "1.8rem",
+        ease: Power1
+      });
+    }
+  };
   return (
-    <Container onClick={onToggleMenu}>
-      <Line>1</Line>
-      <Line>2</Line>
-      <Line>3</Line>
+    <Container onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onClick}>
+      <Line id="ln1">1</Line>
+      <Line id="ln2">2</Line>
+      <Line id="ln3">3</Line>
     </Container>
   );
 };
